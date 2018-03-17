b8ae8941094e55c047ba784487cc0b994674e6a1
DestroyPast <37486951+DestroyPast@users.noreply.github.com>
2018-03-17 23:53:57 +0100

    Simple Introduction Code
    ?Money
    States how much money you have during this session (Starts at 50)
    !GetMoney Gives +1
    !GetMoney 2 Gives +2

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazz_Ass_Hair_Licking_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Money = 50;
+            while (true)
+            {
+                String Imput = Console.ReadLine();
+                if (Imput == "ping")
+                {
+                    Console.WriteLine("pong");
+                }
+                else if (Imput == "?Money")
+                {
+                    Console.WriteLine(Money);
+                }
+                else if (Imput == "!GetMoney")
+                {
+                    Money++;
+                    Console.WriteLine("Now you have:" + Money);
+                }
+                else if (Imput == "!GetMoney 2")
+                {
+                    Money = Money + 2;
+                    Console.WriteLine("Now you have:" + Money);
+
+                }
+            }
+
+        }
+    }
+}
